e133c4f8b212be90efcc02645c31a22aacf335f3
Qin Xiaoming <simon.q.chin@gmail.com>
2017-03-30 22:44:25 +0800

    Add files via upload

--- data_analysis.py
+++ data_analysis.py
@@ -3,10 +3,10 @@
 import pandas as pd
 import numpy as np
 
-ACTION_201602_FILE = "data/JData_Action_201602.csv"
-ACTION_201603_FILE = "data/JData_Action_201603.csv"
-ACTION_201603_EXTRA_FILE = "data/JData_Action_201603_extra.csv"
-ACTION_201604_FILE = "data/JData_Action_201604.csv"
+ACTION_201602_FILE = "data_ori/JData_Action_201602.csv"
+ACTION_201603_FILE = "data_ori/JData_Action_201603.csv"
+ACTION_201603_EXTRA_FILE = "data_ori/JData_Action_201603_extra.csv"
+ACTION_201604_FILE = "data_ori/JData_Action_201604.csv"
 COMMENT_FILE = "data/JData_Comment.csv"
 PRODUCT_FILE = "data/JData_Product.csv"
 USER_FILE = "data/JData_User.csv"
@@ -64,9 +64,55 @@ def merge_weekday_action_data():
     df_ui.columns = ['weekday', 'user_item_num']
     print(df_ui)
 
-merge_weekday_action_data()
+
+def month_action_data_statistic():
+    # Feb.
+    df_ac = get_from_action_data(fname=ACTION_201602_FILE)
+    df_ac['time'] = pd.to_datetime(df_ac['time']).apply(lambda x: x.day)
+
+    # March
+    df_ac = []
+    df_ac.append(get_from_action_data(fname=ACTION_201603_FILE))
+    df_ac.append(get_from_action_data(fname=ACTION_201603_EXTRA_FILE))
+    df_ac = pd.concat(df_ac, ignore_index=True)
+    df_ac['time'] = pd.to_datetime(df_ac['time']).apply(lambda x: x.day)
+
+    # April.
+    df_ac = get_from_action_data(fname=ACTION_201604_FILE)
+    df_ac['time'] = pd.to_datetime(df_ac['time']).apply(lambda x: x.day)
 
 
-df_ac = get_from_action_data(fname=ACTION_201602_FILE)
+def spec_ui_action_data(fname, user_id, item_id, chunk_size=100000):
+    reader = pd.read_csv(fname, header=0, iterator=True)
+    chunks = []
+    loop = True
+    while loop:
+        try:
+            chunk = reader.get_chunk(chunk_size)[
+                ["user_id", "sku_id", "type", "time"]]
+            chunks.append(chunk)
+        except StopIteration:
+            loop = False
+            print("Iteration is stopped")
 
-df_ac['time'] = pd.to_datetime(df_ac['time']).apply(lambda x: x.day)
+    df_ac = pd.concat(chunks, ignore_index=True)
+    df_ac = df_ac[(df_ac['user_id'] == user_id) & (df_ac['sku_id'] == item_id)]
+
+    return df_ac
+
+
+def explore_user_item_via_time():
+    user_id = 10396
+    item_id = 65823
+    df_ac = []
+    df_ac.append(spec_ui_action_data(ACTION_201602_FILE, user_id, item_id))
+    df_ac.append(spec_ui_action_data(ACTION_201603_FILE, user_id, item_id))
+    df_ac.append(spec_ui_action_data(
+        ACTION_201603_EXTRA_FILE, user_id, item_id))
+    df_ac.append(spec_ui_action_data(ACTION_201604_FILE, user_id, item_id))
+
+    df_ac = pd.concat(df_ac, ignore_index=False)
+    print(df_ac.sort_values(by='time'))
+
+
+find_buy_user()
